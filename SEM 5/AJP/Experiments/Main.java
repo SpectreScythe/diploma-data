dc0cc477c556bfa8e8eccb3581e9bfe59e1e52ea
Abdurrahman Qureshi <124421417+SpectreScythe@users.noreply.github.com>
2023-09-19 22:13:53 +0530

    qol

--- SEM 5/AJP/Experiments/Main.java
+++ SEM 5/AJP/Experiments/Main.java
@@ -1,57 +1,50 @@
 import javax.swing.*;
-import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.*;
 
 public class Main {
+    JProgressBar myProgressBar;
+    JLabel JL;
+
     public Main() {
-        JFrame frame = new JFrame("My System Tree");
+        JFrame frame = new JFrame("My Progress Bar");
         frame.setBackground(Color.DARK_GRAY);
         frame.setForeground(Color.WHITE);
 
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode("This PC");
+        Panel P = new Panel();
 
-        DefaultMutableTreeNode folder4 = new DefaultMutableTreeNode("Windows");
-        DefaultMutableTreeNode folder3 = new DefaultMutableTreeNode("Users");
-        DefaultMutableTreeNode folder1 = new DefaultMutableTreeNode("qarq90");
-        DefaultMutableTreeNode folder2 = new DefaultMutableTreeNode("Program Files");
+        JL = new JLabel("0%");
+        myProgressBar = new JProgressBar(1, 500);
+        myProgressBar.setSize(200,200);
 
-        DefaultMutableTreeNode file1 = new DefaultMutableTreeNode("Diploma");
-        DefaultMutableTreeNode file2 = new DefaultMutableTreeNode("Lock Screen");
-        DefaultMutableTreeNode file3 = new DefaultMutableTreeNode("Softwares");
+        P.setLayout(new FlowLayout(1));
 
-        DefaultMutableTreeNode file11 = new DefaultMutableTreeNode("Git");
-        DefaultMutableTreeNode file12 = new DefaultMutableTreeNode("JetBrains");
-        DefaultMutableTreeNode file13 = new DefaultMutableTreeNode("CClearner");
+        P.add(new JLabel("Progress Bar"));
+        P.add(myProgressBar);
+        P.add(JL);
 
-        root.add(folder1);
-        root.add(folder2);
-        root.add(folder3);
-        root.add(folder4);
+        P.setSize(100,100);
 
-        folder1.add(file1);
-        folder1.add(file2);
-        folder1.add(file3);
-
-        folder2.add(file11);
-        folder2.add(file12);
-        folder2.add(file13);
-
-        JTree tree = new JTree(root);
-
-        JScrollPane scrollPane = new JScrollPane(tree);
-
-        tree.setBackground(Color.DARK_GRAY);
-        tree.setForeground(Color.WHITE);
-        tree.setSize(400,300);
-
-        frame.add(scrollPane);
+        frame.add(P);
 
         frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+        loadProgressBar();
+    }
+
+    public void loadProgressBar() {
+        for (int i = 1; i <= 500; i+=5) {
+            myProgressBar.setValue(i);
+            JL.setText(i + "%");
+            try {
+                Thread.sleep(100);
+            } catch (Exception ignored) {
+            }
+        }
     }
 
     public static void main(String[] args) {
-            new Main();
+        new Main();
     }
 }
