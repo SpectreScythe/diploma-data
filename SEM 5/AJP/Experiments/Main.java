e0e0f38f9ea6a911a943e231d80289a12f3ee3d9
Abdurrahman Qureshi <scythethroughtheheart05@gmail.com>
2023-09-30 16:22:53 +0530

    ajp exp

--- SEM 5/AJP/Experiments/Main.java
+++ SEM 5/AJP/Experiments/Main.java
@@ -1,63 +1,24 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.*;
+import java.net.*;
+import java.util.Scanner;
 
 public class Main {
-
-    public Main() {
-        JFrame frame = new JFrame("My Key Events");
-        frame.setBackground(Color.DARK_GRAY);
-        frame.setForeground(Color.WHITE);
-        frame.setLayout(new BorderLayout());
-
-        JPanel P = new JPanel();
-        P.setLayout(new FlowLayout());
-
-        JLabel label1 = new JLabel("Number 1:");
-        JTextField n1 = new JTextField(10);
-
-        JLabel label2 = new JLabel("Number 2:");
-        JTextField n2 = new JTextField(10);
-
-        JPanel buttonPanel = new JPanel();
-        JButton btn = new JButton("Multiply");
-
-        buttonPanel.add(btn);
-
-        JLabel rLabel = new JLabel();
-
-        btn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    double num1 = Double.parseDouble(n1.getText());
-                    double num2 = Double.parseDouble(n2.getText());
-                    double product = num1 * num2;
-                    rLabel.setText("Product: " + product);
-                } catch (NumberFormatException ex) {
-                    rLabel.setText("Invalid input. Please enter valid numbers.");
-                }
-            }
-        });
-
-        P.add(label1);
-        P.add(n1);
-        P.add(label2);
-        P.add(n2);
-
-        frame.add(P, BorderLayout.NORTH);
-        frame.add(buttonPanel, BorderLayout.CENTER);
-        frame.add(rLabel, BorderLayout.SOUTH);
-        frame.setSize(400, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
-    }
-
     public static void main(String[] args) {
-        new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a hostname (e.g., www.google.com): ");
+        String hostname = scanner.nextLine();
+
+        try {
+            InetAddress[] addresses = InetAddress.getAllByName(hostname);
+
+            System.out.println("IP addresses for " + hostname + ":");
+            for (InetAddress address : addresses) {
+                System.out.println(address.getHostAddress());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Unable to resolve hostname: " + hostname);
+        }
+
+        scanner.close();
     }
 }
