777525ab121ba769ccfeb33b10c1f0ee7b53af40
Abdurrahman Qureshi <124421417+SpectreScythe@users.noreply.github.com>
2023-10-27 20:59:13 +0530

    documentation++

--- SEM 5/AJP/Experiments/Main.java
+++ SEM 5/AJP/Experiments/Main.java
@@ -1,28 +1,8 @@
 import java.sql.*;
-public class exp18q2
+public class Main
 {
     public static void main(String args[])
     {
-        try
-        {
-            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-            Connection con=DriverManager.getConnection("jdbc:odbc:exp13");
-            Statement st=con.createStatement();
-
-            ResultSet rs=st.executeQuery("Select * from Student where sper>70");
-
-            System.out.println("Students - Above 75% ");
-            System.out.println("Roll No  \tPercentage");
-            while(rs.next())
-            {
-                System.out.println(rs.getInt(1)+"\t\t"+rs.getInt(3));
-            }
-
-            con.close();
-        }
-        catch(Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
+        System.out.println("Deleted");
     }
 }
